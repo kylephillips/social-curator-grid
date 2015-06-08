a054011e7985b55ec1164b1c93eab27c77205c08
Kyle Phillips <kyle@object9.com>
2015-06-08 16:09:29 -0400

    Added base settings view.

--- app/Views/settings/settings.php
+++ app/Views/settings/settings.php
@@ -1,1 +1,7 @@
-Grid Settings
+<div class="wrap">
+	<h1><?php _e('Social Curator Grid Settings', 'socialcurator'); ?></h1>
+
+	<form method="post" enctype="multipart/form-data" action="options.php">
+		
+	</form>
+</div>
